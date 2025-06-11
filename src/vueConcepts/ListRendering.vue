ed8e9c78377f23ec85e15c1dc8543f915ab1abfd
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-11 15:45:56 +0530

    feat: List Rendering

---
+++ src/vueConcepts/ListRendering.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul v-for="numbers in sets">
+        <li v-for="n in even(numbers)">{{ n }}</li>
+    </ul>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    
+    const sets = ref([
+        [1,2,3,4,5],
+        [6,7,8,9,10]
+    ])
+
+    function even(numbers){
+        return numbers.filter((number) => number % 2 === 0)
+    }
+</script>
