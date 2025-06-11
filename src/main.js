51f44935fd72ab775069eeb32247f2fc2a69424c
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-11 14:08:11 +0530

    vue-create app

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import computed from './computed.vue'
+import Conditional from './Conditional.vue'
+import ListRendering from './ListRendering.vue'
+import ConditionalsLoop from './ConditionalsLoop.vue'
+
+createApp(ConditionalsLoop).mount('#app')
