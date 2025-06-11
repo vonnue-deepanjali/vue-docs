7aef56ecc2e49f953b7642207c60fb6e3c89b2cb
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-11 14:18:21 +0530

    Ref concept

---
+++ src/Ref.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="state.count++">
+      {{ state.count }}
+
+    </button>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0})
+// const count = ref(0)
+
+// console.log(count)
+// console.log(count.value)
+
+// count.value++
+// console.log(count.value)
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import Ref from './Ref.vue'
 import computed from './computed.vue'
 import Conditional from './Conditional.vue'
 import ListRendering from './ListRendering.vue'
 import ConditionalsLoop from './ConditionalsLoop.vue'
 
-createApp(ConditionalsLoop).mount('#app')
+createApp(Ref).mount('#app')
