a9955af7ec9bd59180a2d24e1c64b3ee1be70f89
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-11 15:39:18 +0530

    feat: Conditional Rendering

---
+++ src/vueConcepts/Sum.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>Numbers: {{ data.numbers.join(', ')}}</p>
+    <p>Sum: {{ total }}</p>
+</template>
+
+<script setup>
+    import { reactive, computed } from 'vue'
+
+    const data = reactive({
+        numbers: [10,20,30,40]
+    })
+
+    const total = computed(() => {
+        return data.numbers.reduce((sum, num) => sum + num, 0)
+    })
+</script>
