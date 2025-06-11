3729f8e3663414055ba5eff1f1170ebdfef8889e
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-11 21:25:42 +0530

    update: Form submission

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <Form />
+</template>
+
+<script setup>
+    import Form from './Pages/Form.vue';
+</script>
---
+++ src/Pages/Form.vue
@@ -0,0 +1,35 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label>Name:</label>
+        <input type="text" v-model="name" />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="email" v-model="email" />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" v-model="password" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+
+</template>
+  
+<script setup>
+
+  import { ref } from 'vue'
+  
+  const name = ref('')
+  const email = ref('')
+  const password = ref('')
+  
+  function handleSubmit() {
+    console.log('Name:', name.value)
+    console.log('Email:', email.value)
+    console.log('Password:', password.value)
+  }
+  
+</script>
+  
