1c73c7f56bf6429b31f1df2e369c378a05641c9d
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-12 09:42:55 +0530

    fix: unwanted codes

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Ref from './Ref.vue'
-
-=======
-import App from './App.vue'
->>>>>>> main
-
-=======
 import App from './App.vue'
 
->>>>>>> main
 createApp(App).mount('#app')
