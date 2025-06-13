85ad09f4affbce4e8c13d97fcb420ba7dbb14cd9
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-13 09:52:03 +0530

    update : remove space

--- src/pages/FormValidation.vue
+++ src/pages/FormValidation.vue
@@ -1,23 +1,23 @@
 <template>
-<form @submit.prevent="submitForm">
+  <form @submit.prevent="submitForm">
     <div>
-        <label>Name:</label>
-        <input v-model="name" type="text" />
-        <p v-if="nameError" style="color: red;">{{ nameError }}</p>
+      <label>Name:</label>
+      <input v-model="name" type="text" />
+      <p v-if="nameError" style="color: red">{{ nameError }}</p>
     </div>
 
     <div>
-        <label>Email:</label>
-        <input v-model="email" type="email" />
-        <p v-if="emailError" style="color: red;">{{ emailError }}</p>
+      <label>Email:</label>
+      <input v-model="email" type="email" />
+      <p v-if="emailError" style="color: red">{{ emailError }}</p>
     </div>
 
     <button type="submit" :disabled="nameError || emailError">submit</button>
-</form>
+  </form>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch } from 'vue'
 
 const name = ref('')
 const eamil = ref('')
@@ -26,22 +26,22 @@ const nameError = ref('')
 const emailError = ref('')
 
 watch(name, (newVal) => {
-    if(newVal.length < 3){
-        nameError.value = 'Name must be at least 3 characters'
-    }else{
-        nameError.vlaue ='';
-    }
-});
+  if (newVal.length < 3) {
+    nameError.value = 'Name must be at least 3 characters'
+  } else {
+    nameError.vlaue = ''
+  }
+})
 
 watch(eamil, (newVal) => {
-    if(!newVal.includes('@')){
-        emailError.value = 'Email must include @ symbol'
-    }else{
-        emailError.value = '';
-    }
-});
+  if (!newVal.includes('@')) {
+    emailError.value = 'Email must include @ symbol'
+  } else {
+    emailError.value = ''
+  }
+})
 
 const submitForm = () => {
-    alert('Form submitted successfully')
+  alert('Form submitted successfully')
 }
-</script> 
+</script>
